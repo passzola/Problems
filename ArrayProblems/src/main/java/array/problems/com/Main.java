30084d43d6768c6312b2e2c0b0b5a4ca117a343b
Zelalem <zolaman231@gmail.com>
2025-01-31 20:49:43 -0600

    col check

--- ArrayProblems/src/main/java/array/problems/com/Main.java
+++ ArrayProblems/src/main/java/array/problems/com/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello From Array Problems!");
         System.out.println("Testing!");
+        System.out.println("Testing second check!");
     }
 }
